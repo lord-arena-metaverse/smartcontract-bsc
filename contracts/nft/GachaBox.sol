081ab17de99ba791f291f4db65e94f443cbb7c69
Alex <alex.smith@coinhe.io>
2021-12-28 15:06:55 +0700

    update: gachabox

--- contracts/nft/GachaBox.sol
+++ contracts/nft/GachaBox.sol
@@ -84,16 +84,7 @@ contract GachaBox is Initializable, OwnableUpgradeable {
     }
   }
 
-  function mintCharacters() private returns (uint256[4] memory characterNFTIds) {
-    (uint256[4] memory characterIds, uint256[4] memory characterQualities) = getCharacterIds();
-    for (uint8 i = 0; i < 4; i++) {
-      uint256 characterNFTId =
-        ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characterIds[i], characterQualities[i]);
-      characterNFTIds[i] = characterNFTId;
-    }
-  }
-
-  function getSpecialCharacterNFTId() private returns (uint256) {
+  function getSpecialCharacterNFTId() private returns (uint256 characterNFTId) {
     uint8[26] memory characters =
       [1, 2, 3, 4, 5, 6, 14, 15, 16, 17, 18, 19, 25, 26, 27, 28, 29, 30, 36, 37, 38, 39, 40, 47, 48, 49];
     uint256 qualityId;
@@ -113,57 +104,54 @@ contract GachaBox is Initializable, OwnableUpgradeable {
     }
 
     uint256 characterId = characters[RandomUtil(randomContract).getRandomNumber(characters.length - 1)];
-    uint256 characterNFTId = ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characterId, qualityId);
-    return characterNFTId;
+    characterNFTId = ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characterId, qualityId);
   }
 
-  function genCharacterNFTIdByLORDA() public returns (uint256 characterNFTId) {
+  function genCharacterIdByLORDA() private view returns (uint256 characterId, uint256 qualityId) {
     uint8[8] memory commonCharacters = [12, 23, 34, 44, 13, 24, 35, 45];
     uint8[13] memory rareCharacters = [10, 41, 20, 31, 9, 43, 22, 32, 11, 42, 21, 33, 8];
     uint8[39] memory eliteCharacters =
       [
-        10,
-        41,
-        20,
-        31,
-        9,
-        43,
-        22,
-        32,
-        11,
-        42,
-        21,
-        33,
-        8,
-        3,
-        14,
-        25,
-        36,
-        47,
-        2,
-        15,
-        26,
-        37,
-        48,
         1,
-        16,
-        27,
-        38,
-        49,
+        2,
+        3,
         4,
-        17,
-        29,
-        39,
         5,
-        18,
-        28,
-        40,
         6,
+        8,
+        9,
+        10,
+        11,
+        14,
+        15,
+        16,
+        17,
+        18,
         19,
-        30
+        20,
+        21,
+        22,
+        25,
+        26,
+        27,
+        28,
+        29,
+        30,
+        31,
+        32,
+        33,
+        36,
+        37,
+        38,
+        39,
+        40,
+        41,
+        42,
+        43,
+        47,
+        48,
+        49
       ];
-    uint256 qualityId;
-    uint256 characterId;
 
     uint256 pctRnd = RandomUtil(randomContract).getRandomNumber(100);
     if (pctRnd <= 8) {
@@ -176,8 +164,6 @@ contract GachaBox is Initializable, OwnableUpgradeable {
       qualityId = 2;
       characterId = rareCharacters[RandomUtil(randomContract).getRandomNumber(rareCharacters.length - 1)];
     }
-
-    characterNFTId = ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characterId, qualityId);
   }
 
   function openBoxByBUSD(uint256 _boxId)
@@ -193,9 +179,16 @@ contract GachaBox is Initializable, OwnableUpgradeable {
   {
     require(boxConfig[_boxId].totalSold <= boxConfig[_boxId].quota, "Box is full");
     require(boxConfig[_boxId].price > 0, "Invalid Box.");
-    boxConfig[_boxId].totalSold += _boxId;
+    boxConfig[_boxId].totalSold += 1;
     IERC20Upgradeable(boxConfig[_boxId].currency).transferFrom(msg.sender, treasury, boxConfig[_boxId].price);
-    uint256[4] memory characterNFTIds = mintCharacters();
+
+    uint256[4] memory characterNFTIds;
+    (uint256[4] memory characterIds, uint256[4] memory characterQualities) = getCharacterIds();
+    for (uint8 i = 0; i < 4; i++) {
+      uint256 characterNFTId =
+        ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characterIds[i], characterQualities[i]);
+      characterNFTIds[i] = characterNFTId;
+    }
     character1NFTId = characterNFTIds[0];
     character2NFTId = characterNFTIds[1];
     character3NFTId = characterNFTIds[2];
@@ -206,8 +199,9 @@ contract GachaBox is Initializable, OwnableUpgradeable {
   function openBoxByLORDA(uint256 _boxId) public onlyNonContract returns (uint256 characterNFTId) {
     require(boxConfig[_boxId].totalSold <= boxConfig[_boxId].quota, "Box is full");
     require(boxConfig[_boxId].price > 0, "Invalid Box.");
-    boxConfig[_boxId].totalSold += _boxId;
+    boxConfig[_boxId].totalSold += 1;
     IERC20Upgradeable(boxConfig[_boxId].currency).transferFrom(msg.sender, treasury, boxConfig[_boxId].price);
-    characterNFTId = genCharacterNFTIdByLORDA();
+    (uint256 characterId, uint256 qualityId) = genCharacterIdByLORDA();
+    characterNFTId = ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characterId, qualityId);
   }
 }
