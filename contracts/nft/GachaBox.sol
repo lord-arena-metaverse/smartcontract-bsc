2e5094a974a0d6e288c0963a60ec7d3d70b8b998
Alex <alex.smith@coinhe.io>
2022-01-07 09:14:57 +0700

    update: gachabox

--- contracts/nft/GachaBox.sol
+++ contracts/nft/GachaBox.sol
@@ -24,16 +24,6 @@ contract GachaBox is Initializable, OwnableUpgradeable {
   address public treasury;
   address public buyToken;
   address private randomContract;
-  event NewBoxBUSD(
-    address indexed minter,
-    uint256 character1NFTId,
-    uint256 character2NFTId,
-    uint256 character3NFTId,
-    uint256 character4NFTId,
-    uint256 indexed character5NFTId,
-    uint256 indexed equipmentNFTId
-  );
-  event NewBoxLORDA(address indexed minter, uint256 indexed characterNFTId);
 
   function initialize() public initializer {
     __Ownable_init();
@@ -73,7 +63,7 @@ contract GachaBox is Initializable, OwnableUpgradeable {
     boxConfig[_boxId].maxRewardCardNumber = _maxRewardCardNumber;
   }
 
-  function getSpecialCharacterNFTId() public view returns (uint256, uint256) {
+  function getSpecialCharacterNFTIdByLORDA() public view returns (uint256, uint256) {
     uint8[26] memory characters =
       [1, 2, 3, 4, 5, 6, 14, 15, 16, 17, 18, 19, 25, 26, 27, 28, 29, 30, 36, 37, 38, 39, 40, 47, 48, 49];
     uint256 qualityId;
@@ -83,10 +73,33 @@ contract GachaBox is Initializable, OwnableUpgradeable {
       uint256 highRnd = RandomUtil(randomContract).getRandomNumber(2);
       qualityId = highRnd == 1 ? 9 : 10;
     } else if (pctRnd <= 10) {
-      qualityId = 9;
+      qualityId = 8;
     } else if (pctRnd <= 30) {
       qualityId = 7;
     } else if (pctRnd <= 150) {
+      qualityId = 6;
+    } else {
+      qualityId = 5;
+    }
+
+    uint256 characterId = characters[RandomUtil(randomContract).getRandomNumber(characters.length - 1)];
+    return (characterId, qualityId);
+  }
+
+  function getSpecialCharacterNFTIdByBUSD() public view returns (uint256, uint256) {
+    uint8[26] memory characters =
+      [1, 2, 3, 4, 5, 6, 14, 15, 16, 17, 18, 19, 25, 26, 27, 28, 29, 30, 36, 37, 38, 39, 40, 47, 48, 49];
+    uint256 qualityId;
+
+    uint256 pctRnd = RandomUtil(randomContract).getRandomNumber(10000);
+    if (pctRnd <= 25) {
+      uint256 highRnd = RandomUtil(randomContract).getRandomNumber(2);
+      qualityId = highRnd == 1 ? 9 : 10;
+    } else if (pctRnd <= 150) {
+      qualityId = 8;
+    } else if (pctRnd <= 300) {
+      qualityId = 7;
+    } else if (pctRnd <= 1000) {
       qualityId = 6;
     } else {
       qualityId = 5;
@@ -155,10 +168,7 @@ contract GachaBox is Initializable, OwnableUpgradeable {
     }
   }
 
-  function openBoxByBUSD()
-    public
-    onlyNonContract
-  {
+  function openBoxByLORDA() public onlyNonContract {
     require(boxConfig[1].totalSold <= boxConfig[1].quota, "Box is full");
     require(boxConfig[1].price > 0, "Invalid Box.");
     boxConfig[1].totalSold += 1;
@@ -178,21 +188,41 @@ contract GachaBox is Initializable, OwnableUpgradeable {
         characterQuality = 3;
       }
       ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characters[characterIdx], characterQuality);
+      uint256 equipmentId = RandomUtil(randomContract).getRandomNumber(12);
+      ILordArenaEquipment(lordArenaItem).safeMint(msg.sender, equipmentId, 4);
     }
-    (uint256 nftID, uint256 qualityId) = getSpecialCharacterNFTId();
+    (uint256 nftID, uint256 qualityId) = getSpecialCharacterNFTIdByLORDA();
     ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, nftID, qualityId);
     uint256 itemId = RandomUtil(randomContract).getRandomNumber(12);
     ILordArenaEquipment(lordArenaItem).safeMint(msg.sender, itemId, 4);
-    // emit NewBoxBUSD(msg.sender, character1NFTId, character2NFTId, character3NFTId, character5NFTId, equipmentNFTId);
   }
 
-  function openBoxByLORDA() public onlyNonContract returns (uint256 characterNFTId) {
+  function openBoxByBUSD() public onlyNonContract {
     require(boxConfig[2].totalSold <= boxConfig[2].quota, "Box is full");
     require(boxConfig[2].price > 0, "Invalid Box.");
     boxConfig[2].totalSold += 1;
     IERC20Upgradeable(boxConfig[2].currency).transferFrom(msg.sender, treasury, boxConfig[2].price);
-    (uint256 characterId, uint256 qualityId) = genCharacterIdByLORDA();
-    characterNFTId = ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characterId, qualityId);
-    emit NewBoxLORDA(msg.sender, characterNFTId);
+
+    uint256[13] memory characters = [uint256(8), 9, 10, 11, 20, 21, 22, 31, 32, 33, 41, 42, 43];
+
+    for (uint256 i = 0; i < 4; i++) {
+      uint256 characterQuality;
+      uint256 characterIdx = RandomUtil(randomContract).getRandomNumber(characters.length - i);
+      characters[characterIdx] = characters[characters.length - i - 1];
+
+      uint256 pctRnd = RandomUtil(randomContract).getRandomNumber(100);
+      if (pctRnd <= 70) {
+        characterQuality = 2;
+      } else {
+        characterQuality = 3;
+      }
+      ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, characters[characterIdx], characterQuality);
+      uint256 equipmentId = RandomUtil(randomContract).getRandomNumber(12);
+      ILordArenaEquipment(lordArenaItem).safeMint(msg.sender, equipmentId, 4);
+    }
+    (uint256 nftID, uint256 qualityId) = getSpecialCharacterNFTIdByBUSD();
+    ILordArenaCharacter(lordArenaCharacter).safeMint(msg.sender, nftID, qualityId);
+    uint256 itemId = RandomUtil(randomContract).getRandomNumber(12);
+    ILordArenaEquipment(lordArenaItem).safeMint(msg.sender, itemId, 4);
   }
 }
